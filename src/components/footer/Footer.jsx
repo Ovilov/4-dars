517ef0b9ecb8146973044480fc7cf2fbb20b2be3
Ovilov <ovilovsaidakbar@gmail.com>
2024-05-31 02:19:54 +0500

    Create User web site in React  add

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// style
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="https://t.me/ovilovsaidakbar20020425">Saidakbar Ovilov</a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
